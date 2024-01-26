625e3388760116deca381bf532a619d3ebd815dc
Cole Mueller <coleaaronmueller@gmail.com>
2024-01-26 16:35:40 -0700

    Unity tool to auto-initialize a 2D softbody sprite

---
+++ CreateSoftbody2D.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.U2D.Animation;
+using System;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+
+public class CreateSoftbody2D : MonoBehaviour
+{
+    [MenuItem("Custom Tools/Create Softbody 2D")]
+    static void MakeSoftbody()
+    {
+        Transform _selection = Selection.activeTransform;
+
+        //check if current selection has "Sprite Skin" script and bones as childern
+        SpriteSkin _spriteSkinScr = _selection.GetComponent<SpriteSkin>();
+        if(_spriteSkinScr == null)
+        {
+            Debug.LogError("Create Softbody 2D :: Current selection does not have a \"Sprite Skin\" component.");
+            return;
+        }
+        if(_spriteSkinScr.rootBone == null || _spriteSkinScr.boneTransforms.Length == 0)
+        {
+            Debug.LogError("Create Softbody 2D :: Current selection does not have any bone objects.");
+            return;
+        }
+        
+        //warn for uneven # of bones
+        if(_spriteSkinScr.boneTransforms.Length % 2 != 0) Debug.LogWarning("Create Softbody 2D :: Selected object has an uneven number of bones. Unexpected physics behaviors may occur.");
+        
+        //add rigidbodies and colliders
+        Transform[] _bones = _spriteSkinScr.boneTransforms;
+        foreach(Transform child in _bones)
+        {
+            if(!child.GetComponent<Rigidbody2D>()) child.AddComponent<Rigidbody2D>();
+            if(!child.GetComponent<CircleCollider2D>())
+            {
+                CircleCollider2D _col = child.AddComponent<CircleCollider2D>();
+
+                _col.radius = 0.04f;
+                _col.offset = new Vector2(0.01f, 0);
+            }
+        }
+
+        /*This block adds spring joint to each bone and assigns appropriate rigidbodies + values to get the default behavior I want. I will manually tweak frequency and dampening ratio to get the stiffness I want later. */
+        int _oppositeOffset = _bones.Length / 2;
+        for(int j = 0; j < _bones.Length; j++)
+        {
+            SpringJoint2D _leftSpring = _bones[j].AddComponent<SpringJoint2D>();
+            SpringJoint2D _rightSpring = _bones[j].AddComponent<SpringJoint2D>();
+            SpringJoint2D _oppositeSpring = _bones[j].AddComponent<SpringJoint2D>();
+
+            _leftSpring.autoConfigureConnectedAnchor = _rightSpring.autoConfigureConnectedAnchor = _oppositeSpring.autoConfigureConnectedAnchor = true;
+
+            if(j == 0)
+            {
+                _leftSpring.connectedBody = _bones[^1].GetComponent<Rigidbody2D>();
+                _rightSpring.connectedBody = _bones[1].GetComponent<Rigidbody2D>();
+            }
+            else if(j == _bones.Length - 1)
+            {
+                _leftSpring.connectedBody = _bones[j-1].GetComponent<Rigidbody2D>();
+                _rightSpring.connectedBody = _bones[0].GetComponent<Rigidbody2D>();
+            }
+            else
+            {
+                _leftSpring.connectedBody = _bones[j-1].GetComponent<Rigidbody2D>();
+                _rightSpring.connectedBody = _bones[j+1].GetComponent<Rigidbody2D>();
+            }
+
+            if(j < _oppositeOffset)
+            {
+                _oppositeSpring.connectedBody = _bones[j + _oppositeOffset].GetComponent<Rigidbody2D>();
+            }
+            else
+            {
+                _oppositeSpring.connectedBody = _bones[j - _oppositeOffset].GetComponent<Rigidbody2D>();
+            }
+
+            _leftSpring.autoConfigureDistance = _rightSpring.autoConfigureDistance = _oppositeSpring.autoConfigureDistance = true;
+            _leftSpring.autoConfigureConnectedAnchor = _rightSpring.autoConfigureConnectedAnchor = _oppositeSpring.autoConfigureConnectedAnchor = false;
+
+            _leftSpring.dampingRatio = _rightSpring.dampingRatio = _oppositeSpring.dampingRatio = 0.3f;
+            _leftSpring.frequency = _rightSpring.frequency = _oppositeSpring.frequency = 5f;
+        }
+
+        Debug.Log("Create Softbody 2D :: Selected object skinned successfully!");
+    }
+}
