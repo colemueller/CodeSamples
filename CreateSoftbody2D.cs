1a52816b9ff55dae6f35164e0bb4241f6c05b866
Cole Mueller <coleaaronmueller@gmail.com>
2024-01-26 17:50:21 -0700

    Unity tool to auto-initialize a 2D softbody sprite * Tweaked default values

--- CreateSoftbody2D.cs
+++ CreateSoftbody2D.cs
@@ -18,29 +18,33 @@ public class CreateSoftbody2D : MonoBehaviour
         SpriteSkin _spriteSkinScr = _selection.GetComponent<SpriteSkin>();
         if(_spriteSkinScr == null)
         {
-            Debug.LogError("Create Softbody 2D :: Current selection does not have a \"Sprite Skin\" component.");
+            Debug.LogError("Create Softbody 2D :: Current selection (" + _selection.name + ") does not have a \"Sprite Skin\" component.");
             return;
         }
         if(_spriteSkinScr.rootBone == null || _spriteSkinScr.boneTransforms.Length == 0)
         {
-            Debug.LogError("Create Softbody 2D :: Current selection does not have any bone objects.");
+            Debug.LogError("Create Softbody 2D :: Current selection (" + _selection.name + ") does not have any bone objects.");
             return;
         }
         
         //warn for uneven # of bones
-        if(_spriteSkinScr.boneTransforms.Length % 2 != 0) Debug.LogWarning("Create Softbody 2D :: Selected object has an uneven number of bones. Unexpected physics behaviors may occur.");
+        if(_spriteSkinScr.boneTransforms.Length % 2 != 0) Debug.LogWarning("Create Softbody 2D :: Selected object (" + _selection.name + ") has an uneven number of bones. Unexpected physics behaviors may occur.");
         
         //add rigidbodies and colliders
         Transform[] _bones = _spriteSkinScr.boneTransforms;
         foreach(Transform child in _bones)
         {
-            if(!child.GetComponent<Rigidbody2D>()) child.AddComponent<Rigidbody2D>();
+            if(!child.GetComponent<Rigidbody2D>())
+            {
+                Rigidbody2D _rb = child.AddComponent<Rigidbody2D>();
+                _rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+                _rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+            }
             if(!child.GetComponent<CircleCollider2D>())
             {
                 CircleCollider2D _col = child.AddComponent<CircleCollider2D>();
 
-                _col.radius = 0.04f;
-                _col.offset = new Vector2(0.01f, 0);
+                _col.radius = 0.1f;
             }
         }
 
@@ -53,6 +57,7 @@ public class CreateSoftbody2D : MonoBehaviour
             SpringJoint2D _oppositeSpring = _bones[j].AddComponent<SpringJoint2D>();
 
             _leftSpring.autoConfigureConnectedAnchor = _rightSpring.autoConfigureConnectedAnchor = _oppositeSpring.autoConfigureConnectedAnchor = true;
+            _leftSpring.autoConfigureDistance = _rightSpring.autoConfigureDistance = _oppositeSpring.autoConfigureDistance = false;
 
             if(j == 0)
             {
@@ -79,13 +84,12 @@ public class CreateSoftbody2D : MonoBehaviour
                 _oppositeSpring.connectedBody = _bones[j - _oppositeOffset].GetComponent<Rigidbody2D>();
             }
 
-            _leftSpring.autoConfigureDistance = _rightSpring.autoConfigureDistance = _oppositeSpring.autoConfigureDistance = true;
-            _leftSpring.autoConfigureConnectedAnchor = _rightSpring.autoConfigureConnectedAnchor = _oppositeSpring.autoConfigureConnectedAnchor = false;
+            _leftSpring.distance = _rightSpring.distance = _oppositeSpring.distance = 0.05f;
 
             _leftSpring.dampingRatio = _rightSpring.dampingRatio = _oppositeSpring.dampingRatio = 0.3f;
             _leftSpring.frequency = _rightSpring.frequency = _oppositeSpring.frequency = 5f;
         }
 
-        Debug.Log("Create Softbody 2D :: Selected object skinned successfully!");
+        Debug.Log("Create Softbody 2D :: Selected object (" + _selection.name + ") skinned successfully!");
     }
 }
